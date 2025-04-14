a04b05fd9508b860c5f772c866338a3c299515c0
Kufzuk2 <vologin.vv@phystech.edu>
2025-04-15 00:03:57 +0300

    2nd task aboutt the dir path seems to work correct

--- task1/dir_path.c
+++ task1/dir_path.c
@@ -1,131 +1,81 @@
-
-#include "list.h"
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <limits.h>
 
+#define PATH_MAX 4096
+#define MAX_DIRS 1024 
 
-int main(){
+char* simpl_path(char* path) 
+{
+    char* components[MAX_DIRS];
+    int top = -1;  
 
-    max_len = 4096;
-    char  input_path [max_len + 1];
-    char output_path [max_len + 1];
-    
-    fgets(input, max_len, stdin);
-    input[strcspn(input, "\n")] = '\0';
-`
-    get_path(input_path, output_path);
-
-    printf("%s \n", output_path);
-}
-
-/*
-void get_path(char* input_path, char* output_path){
-
-    char* div = "/";
-    if (!strcmp(input_path[0], '/')) {
-        printf("path must start from '/' \n");
-        return;
-    }
-    list* path = (list*) malloc(sizeof(list)); 
-   
-
-    char* dir;
-    char* path_copy = strdup(path); 
-    dir = strtok(path_copy, delimiter);
-
-    while (dir) {
-        if (strcmp(dir, ".") == 0) {
-
-        } else if (strcmp(dir, "..") == 0) {
-            if (list->head != NULL) {
-                remove_last(list);
-            } else {
-                printf("Going upper than the root directory is not possible.\n");
-                free(path_copy);
-                free_list(list);
-                return "/";
-            }
-        } else {
-            add(list, dir);
-        }
-        dir = strtok(NULL, delimiter);
-    }
-}
-
-*/
-
-
-
-void add_dir(list** path, char* dir) {
-    if (!dir || strlen(dir) == 0) 
-        return;
-
-    *path = add(*path, strdup(dir));
-}
-
-
-void pop_dir(list** path) {   // kill list 
-    if (*path == NULL) 
-        return;
-
-    list* tmp = *path;
-    *path = (*path)->prev_;
-    free(tmp->data_);
-    free(tmp);
-}
-
-
-
-
-char* get_path(char* init_path) {
-    list* path = NULL;
-
-    char* path_copy = strdup(init_path);
-    char* token     = strtok(path_copy, "/");
-
-    while (token != NULL) {
-        if (!strcmp(token, ".")) {}
-
-        else if (!strcmp(token, "..")) {
-            if (path == NULL) {
-                printf("Going upper than the root directory is not possible.\n");
-
-                free(path_copy);
-                kill_list(path);
-
-                return "/";
-            }
-            
-            pop_dir(&path);
+    char* token = strtok(path, "/");
+    while (token != NULL) 
+    {
+        if (strcmp(token, "..") == 0) 
+        {
+            if (top >= 0) 
+                free(components[top--]);
         } 
-        else {
-            add_dir(&path, token);
-        }
+        else if (strcmp(token, ".") != 0 && strlen(token) > 0) 
+        {
+            if (top + 1 >= MAX_DIRS) 
+            {
+                fprintf(stderr, "Path is too deep\n");
+                return NULL;
+            }
 
+            components[++top] = strdup(token);
+            if (components[top] == NULL) 
+            {
+                fprintf(stderr, "Memory allocation failed\n");
+                return NULL;
+            }
+        }
         token = strtok(NULL, "/");
     }
 
+    char* result = malloc(PATH_MAX);
+    if (result == NULL) 
+    {
+        fprintf(stderr, "Memory allocation failed\n");
+        return NULL;
+    }
+    result[0] = '\0';
 
-    char  path[PATH_MAX];
-    out_path[0]   = '\0'; 
-
-    list* current = path;
-
-    while (current) {
-        if (strlen(out_path) > 0) {
-            strcat(out_path, "/");
+    if (top == -1) 
+        strcpy(result, "/");
+    else 
+    {
+        for (int i = 0; i <= top; i++) 
+        {
+            strcat(result, "/");
+            strcat(result, components[i]);
+            free(components[i]);  
         }
-        strcat(out_path, current->data_);
-        current = current->prev_;
     }
 
-    if (strlen(out_path) == 0) {
-        strcpy(out_path, "/");
-    }
-
-    free(path_copy);
-    kill_list(path);
-
-    return strdup(out_path);
+    return result;
 }
 
+int main()
+{
+    char path[PATH_MAX];
+    if (fgets(path, PATH_MAX, stdin) == NULL) 
+        return 1;
+    
 
+    path[strcspn(path, "\n")] = '\0';
+
+    char* canon_path = simpl_path(path);
+    
+    if (canon_path != NULL) {
+        printf("%s\n", canon_path);
+        free(canon_path);
+    } else 
+        return 1;
+
+    return 0;
+}
